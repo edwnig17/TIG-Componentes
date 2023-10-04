dd79f15ed13bded453efc3ac6d62ffcf9bb6c8c3
edwnig17 <edwingstiven20@gmail.com>
2023-10-03 21:36:44 -0500

    calculadora 100% funcional ⚛️⚛️

---
+++ calculadora-componentes/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+document.title = 'Edwing Calculadora matemática' //Llamamos al document.title para poder asignar un title a nuestro pagina web 
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
